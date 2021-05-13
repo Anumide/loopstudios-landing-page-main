efaf9fd64e6ae71a5ffef0542c24b4876c82505c
Anumide <alabasamuelayomide3@gmail.com>
2021-05-13 14:41:28 +0100

    design process

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,34 @@ module.exports = {
         customjosefin: ['Josefin Sans', 'sans-serif']
       },
       colors: {
-        
+        customwhite: 'hsl(0, 0%, 100%)',
+        customblack: 'hsl(0, 0%, 0%)',
+        customgray: {
+          '100': 'hsl(0, 0%, 55%)',
+          '200': 'hsl(0, 0%, 41%)',
+        },
       },
-
+      backgroundImage: {
+        headbackmbl: 'linear-gradient(rgba(00, 00, 00, .3), rgba(00, 00, 00, .3)), url("./images/mobile/image-hero.jpg")',
+        headbackdskp: 'linear-gradient(rgba(00, 00, 00, .3), rgba(00, 00, 00, .3)), url("./images/desktop/image-hero.jpg")',
+        dpearthmbl: 'url("./images/mobile/image-deep-earth.jpg")',
+        dpearthdskp: 'url("./images/desktop/image-deep-earth.jpg")',
+        nghtarcmbl: 'url("./images/mobile/image-night-arcade.jpg")',
+        nghtarcdskp: 'url("./images/desktop/image-night-arcade.jpg")',
+        soctmbl: 'url("./images/mobile/image-soccer-team.jpg")',
+        soctdskp: 'url("./images/desktop/image-soccer-team.jpg")',
+        girdmbl: 'url("./images/mobile/images/image-grid.jpg")',
+        girddskp: 'url("./images/desktop/images/image-grid.jpg")',
+        frmabvmbl: 'url("./images/mobile/images/image-from-above.jpg")',
+        frmabvdskp: 'url("./images/desktop/images/image-from-above.jpg")',
+        pktmbl: 'url("./images/mobile/image-pocket-borealis.jpg")',
+        pktdskp: 'url("./images/desktop/image-pocket-borealis.jpg")',
+        curiousmbl: 'url("./images/mobile/image-curiosity.jpg")',
+        curiousdskp: 'url("./images/desktop/image-curiosity.jpg")',
+        madeitmbl: 'url("./images/mobile/image-fisheye.jpg")',
+        madeitdskp: 'url("./images/desktop/image-fisheye.jpg")',
+      },
+      
 
     },
   },
